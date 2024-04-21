f5cc6d0beb2f37fc6fed5acb6621ad1bfc76e4ec
Lee jieun <wldmsdl713@gmail.com>
2024-04-21 15:41:10 +0900

    프로젝트 완

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import TodoHeader from './Components/TodoHeader'
+import TodoInput from './Components/TodoInput';
+import TodoList from './Components/TodoList';
+
+function fetchTodos() {
+  const result = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const value = localStorage.key(i);
+    result.push(value);
+  }
+  return result;
+}
+
+function App() {
+
+  const [todos, setTodos] = useState(fetchTodos());
+
+  const addTodo = (todo) => {
+    localStorage.setItem(todo, todo);
+    setTodos((currentTodos) => {
+      return [...currentTodos, todo]
+    })
+  }
+
+  const removeTodo = (todo) => {
+    const result = todos.filter(todoItem => {
+      if (todoItem !== todo) {
+        return true;
+      }
+    })
+    setTodos(result);
+    localStorage.removeItem(todo);
+  }
+
+  return (
+    <div>
+      <TodoHeader />
+      <TodoInput onTodoAdd={addTodo} />
+      <TodoList todos={todos} onTodoRemove={removeTodo} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/TodoHeader.jsx
@@ -0,0 +1,9 @@
+function TodoHeader() {
+    return (
+        <div className='TodoHeader'>
+            <h1>투두 리스트</h1>
+        </div>
+    )
+}
+
+export default TodoHeader
